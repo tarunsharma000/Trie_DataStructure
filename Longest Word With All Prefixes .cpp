39aff22669eb149c232bead5738bb1be33e75850
TARUN SHARMA <87997250+tarunsharma000@users.noreply.github.com>
2022-01-30 23:33:39 +0530

    Update

--- Longest Word With All Prefixes .cpp
+++ Longest Word With All Prefixes .cpp
@@ -1,119 +1,71 @@
-#include<bits/stdc++.h>
-using namespace std;
-struct Node
-{
-    Node *links[26];
-    bool flag=false;
-
-    bool containKey(char ch)
-    {
-        if(links[ch-'a']!=NULL)
-        {
-            return true;
+struct Node {
+    Node *links[26]; 
+    bool flag = false; 
+    
+    bool containsKey(char ch) {
+        return (links[ch - 'a'] != NULL); 
+    }
+    Node* get(char ch) {
+        return links[ch-'a']; 
+    }
+    void put(char ch, Node* node) {
+        links[ch-'a'] = node; 
+    }
+    void setEnd() {
+        flag = true; 
+    }
+    bool isEnd() {
+        return flag; 
+    }
+}; 
+class Trie {
+    private: Node *root; 
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new Node(); 
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Node *node = root;
+        for(int i = 0;i<word.size();i++) {
+            if(!node->containsKey(word[i])) {
+                node->put(word[i], new Node()); 
+            }
+            node = node->get(word[i]); 
         }
-        return false;
+        node->setEnd(); 
     }
-    void putKey(char ch,Node* node)
-    {
-        links[ch-'a']=node;
-    }
-    Node* getNode(char ch)
-    {
-        return links[ch-'a'];
-    }
-    void setEnd()
-    {
-        flag=true;
-    }
-    bool isend()
-    {
-        return flag;
+    bool checkIfAllPrefixExists(string word) {
+        Node *node = root;
+        bool flag = true; 
+        for(int i = 0;i<word.size();i++) {
+            if(node->containsKey(word[i])) {
+                node = node->get(word[i]); 
+                flag = flag & node->isEnd(); 
+            }
+            else {
+                return false; 
+            } 
+        }
+        return flag; 
     }
 };
-class Trie
-{
-    private: Node* root;
-    public:
-    Trie()
-    {
-        root=new Node();
-    }
-    void insert(string word)
-    {
-        int len=word.length();
-        Node* node=root;
-        for(int i=0;i<len;i++)
-        {
-            if(!node->containKey(word[i]))
-            {
-                node->putKey(word[i],new Node());
+string completeString(int n, vector<string> &a){
+    Trie* obj = new Trie();
+    for(auto word : a) obj->insert(word); 
+    string longest = ""; 
+    for(auto word: a) {
+        if(obj->checkIfAllPrefixExists(word)) {
+            if(word.size() > longest.size()) {
+                longest = word; 
             }
-            node=node->getNode(word[i]);
-        }
-        node->setEnd();
-    }
-    bool search(string word)
-    {
-        int len=word.length();
-        Node* node=root;
-        for(int i=0;i<len;i++)
-        {
-            if(!node->containKey(word[i]))
-            {
-               return false;
+            else if(word.size() == longest.size() && word < longest) {
+                longest = word; 
             }
-            node=node->getNode(word[i]);
         }
-        if(node->isend()==true)
-        {
-            return true;
-        }
-        return false;
     }
-     bool startwith(string word)
-    {
-        int len=word.length();
-        Node* node=root;
-        for(int i=0;i<len;i++)
-        {
-            if(!node->containKey(word[i]))
-            {
-               return false;
-            }
-            node=node->getNode(word[i]);
-        }
-        return true;
-    }
-};
-int main()
-{
-   Trie* obj=new Trie();
-   int n;
-   cin>>n;
-   string A[n];
-   for(int i=0;i<n;i++)
-   {
-       cin>>A[i];
-       obj->insert(A[i]);
-   }
-   int ans=INT_MIN;
-   for(int i=0;i<n;i++)
-   {
-       string temp=A[i];
-       bool flag=true;
-       for(int i=0;i<temp.length();i++)
-       {
-           string check=temp.substr(0,i+1);
-           if(!obj->search(check))
-           {
-               flag=false;
-              break;
-           }
-       }
-       if(flag)
-       {
-           ans=max(ans,(int)temp.length());
-       }
-   }
-    return 0;
+    if(longest == "") return "None"; 
+    return longest; 
 }
